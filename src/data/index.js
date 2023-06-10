f6c696c47cdca50eb1191a3a282ac26f55b6280b
Zareel Kalam <zareelbwd@gmail.com>
2023-06-10 19:58:22 +0530

    cv attached

--- src/data/index.js
+++ src/data/index.js
@@ -71,7 +71,7 @@ export const homeLinks = [
         Resume <ContactsIcon className="ml-8" />
       </span>
     ),
-    href: "/public/ZareelCV.pdf",
+    href: "/public/zareelcv.pdf",
     style: "rounded-br-md",
     download: true,
   },
